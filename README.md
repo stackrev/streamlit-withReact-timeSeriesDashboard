6875c9f5ff6e12ab7cfa8a83d885d0fa49c17d17
stackrev <farbitherast5888@gmail.com>
2022-04-05 11:19:40 +0200

    TS and React

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Installation and Build
 
 Create conda env: `conda create --name ts python=3.9`
+
+go to frontend and run `yarn build` this will create a dist folder which will be accessed by the streamlit.
+
+go to streamlit, install all requirements `conda install --file requirements.txt`
+run the streamlit: `streamlit run app.py`
---
+++ frontend/src/App.tsx
@@ -0,0 +1,78 @@
+import {
+  Streamlit,
+  StreamlitComponentBase,
+  withStreamlitConnection,
+} from 'streamlit-component-lib';
+import React, { ReactNode } from 'react';
+
+import Highcharts from 'highcharts/highstock';
+import HighchartsReact from 'highcharts-react-official';
+import Indicators from 'highcharts/indicators/indicators-all.js';
+import DragPanes from 'highcharts/modules/drag-panes.js';
+import AnnotationsAdvanced from 'highcharts/modules/annotations-advanced.js';
+import PriceIndicator from 'highcharts/modules/price-indicator.js';
+import FullScreen from 'highcharts/modules/full-screen.js';
+import StockTools from 'highcharts/modules/stock-tools.js';
+
+import './App.css';
+
+import APPL_TS from './aapl-c.json';
+
+// init the module
+Indicators(Highcharts);
+DragPanes(Highcharts);
+AnnotationsAdvanced(Highcharts);
+PriceIndicator(Highcharts);
+FullScreen(Highcharts);
+StockTools(Highcharts);
+
+const options = {
+  yAxis: [
+    {
+      height: '80%',
+    },
+    {
+      top: '80%',
+      height: '20%',
+      offset: 0,
+    },
+  ],
+  rangeSelector: {
+    selected: 1,
+  },
+  title: {
+    text: 'AAPL Stock Price',
+  },
+  series: [
+    {
+      name: 'AAPL',
+      data: APPL_TS,
+      tooltip: {
+        valueDecimals: 2,
+      },
+    },
+  ],
+};
+
+interface State {
+  data: number[][];
+}
+
+class App extends StreamlitComponentBase<State> {
+  public state = { data: APPL_TS };
+
+  public render = (): ReactNode => {
+    return (
+      <div>
+        <HighchartsReact
+          highcharts={Highcharts}
+          constructorType={'stockChart'}
+          options={options}
+        />
+      </div>
+    );
+  };
+}
+
+//export default App; // run this as standalone.
+export default withStreamlitConnection(App);
